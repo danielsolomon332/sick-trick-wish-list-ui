f9f65b88e0d6490728c203795d42872074049de7
danielsolomon332 <daniel.solomon332@gmail.com>
2022-04-06 10:50:12 -0600

    Build Card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import './Card.css';
 
+const Card = ({ stance, name, obstacle, tutorial, id}) => {
+  return (
+    <div className='card'>
+      <h2>{name}</h2>
+      <h3>{stance}</h3>
+      <p>{obstacle}</p>
+      <section>{tutorial}</section>
+    </div>
+  )
+}
+
+export default Card;
