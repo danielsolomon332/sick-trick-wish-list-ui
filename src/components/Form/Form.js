7c27257f2ed94ede6929c316824e1159ca67c545
danielsolomon332 <daniel.solomon332@gmail.com>
2022-04-06 12:10:01 -0600

    Create From component and add selections

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,9 +5,66 @@ class Form extends Component {
   constructor() {
     super();
     this.state = {
-      title: '',
-      description: ''
+      stance: '',
+      name: '',
+      obstacle: '',
+      tutorial: ''
     }
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  submitTrick = event => {
+    event.preventDefault()
+
+    const newTrick = {
+      id: Date.now(),
+      ...this.state
+    }
+    this.props.addTrick(newTrick)
+
+    this.clearInputs()
+  }
+
+  clearInputs = () => {
+    this.setState({
+      stance: '',
+      name: '',
+      obstacle: '',
+      tutorial: ''
+    })
+  }
+
+  render() {
+    return (
+      <form>Add a trick!
+        <input
+          type='text'
+          placeholder='Trick Name'
+          name='trick name'
+          value={this.state.name}
+          onChange={event => this.handleChange(event)}
+        />
+
+        <select>Stance:
+          <option value="regular">Regular</option>
+          <option value="switch">Switch</option>
+        </select>
+
+        <select>Obstacle:
+          <option value="flatground">flatground</option>
+          <option value="ledge">ledge</option>
+          <option value="rail">rail</option>
+          <option value="stairs">stairs</option>
+          <option value="pool">pool</option>
+        </select>
+
+        <button onClick={event =>
+        this.submitTrick(event)}>Submit</button>
+      </form>
+    )
   }
 }
 
