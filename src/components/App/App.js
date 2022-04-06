9fb4da3f449c293f02193ba533424b762852621b
danielsolomon332 <daniel.solomon332@gmail.com>
2022-04-06 10:41:44 -0600

    Add imports and functions to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,34 @@
 import { Component } from 'react';
+import Card from '../Card/Card';
+import Tricks from '../Tricks/Tricks';
 import './App.css';
+import fetchDataGet from '../../APIcalls'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Sick Trick Wish List</h1>
-      </div>
-    );
+  constructor() {
+    super()
+    this.state = {
+      tricks: null
+    }
   }
+
+  componentDidMount() {
+    fetchDataGet('tricks')
+      .then((data) => {
+        this.setState({
+          tricks: data
+        })
+      })
+  }
+
+    render() {
+      return (
+        <main className="App">
+          <h1>Sick Trick Wish List</h1>
+          <Card tricks={this.state.tricks} />
+        </main>
+      )
+    }
 }
 
 export default App;
